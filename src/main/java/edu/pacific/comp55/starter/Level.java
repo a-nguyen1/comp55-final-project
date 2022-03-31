c6008b307e40ae960077a48db873cfc4ea158e41
Austin <a_nguyen202@u.pacific.edu>
2022-03-31 11:53:29 -0700

    added private variables

---
+++ src/main/java/edu/pacific/comp55/starter/Level.java
@@ -0,0 +1,18 @@
+package edu.pacific.comp55.starter;
+import java.util.ArrayList; // for ArrayList
+
+public class Level {
+	private ArrayList<Room> rooms;
+	private int currentRoom;
+	private int level;
+	
+	public Level() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
