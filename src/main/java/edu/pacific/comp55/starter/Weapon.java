31233e80736ab2556b15c1d4d0b6ee8c84f6f8f6
Austin <a_nguyen202@u.pacific.edu>
2022-03-31 13:39:27 -0700

    added Weapon class

---
+++ src/main/java/edu/pacific/comp55/starter/Weapon.java
@@ -0,0 +1,15 @@
+package edu.pacific.comp55.starter;
+
+public class Weapon extends Item {
+	private int range;
+	
+	public Weapon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
