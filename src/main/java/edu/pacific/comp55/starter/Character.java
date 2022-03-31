eb534dbfdc477f5d212f37ace641e4ce24155ff5
Austin <a_nguyen202@u.pacific.edu>
2022-03-31 11:48:52 -0700

    added character class

---
+++ src/main/java/edu/pacific/comp55/starter/Character.java
@@ -0,0 +1,41 @@
+package edu.pacific.comp55.starter;
+import acm.graphics.GImage;
+
+public class Character {
+	private GImage sprite;
+	private int health;
+	
+	public Character() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void healthChanged(int h) { //TODO implement health changed
+		
+	}
+	
+	public boolean healthIsZero() { //TODO implement health is zero
+		return true;
+	}
+	
+	public int getHealth(int h) {
+		return health;
+	}
+	
+	public void setHealth(int h) {
+		health = h;
+	}
+	
+	public void move() { //TODO implement movement
+		
+	}
+	
+	public boolean isHit() { //TODO implement is hit
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
