670caca67427444651d8facd6368e704bd7b4fed
Austin <a_nguyen202@u.pacific.edu>
2022-03-31 11:05:36 -0700

    added Room class and removed unneeded images

---
+++ src/main/java/edu/pacific/comp55/starter/Room.java
@@ -0,0 +1,36 @@
+package edu.pacific.comp55.starter;
+import java.util.ArrayList; // for arraylist
+import javax.swing.*; // for timer
+
+public class Room {
+	private Timer timer;
+	private String roomType;
+	//private Player player;
+	//private ArrayList<Item> items;
+	
+	public Room() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void addPlayer() { //TODO add player to room
+		
+	}
+	
+	public void addEnemies() { //TODO add enemies to room
+		
+	}
+	
+	public void addItems() { //TODO add items to room
+		
+	}
+	
+	public void nextRoom() { //TODO move to next room
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
