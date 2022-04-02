b7159f707842386320eba4d80e3d8aca8bcdb40c
Melkman68 <e_khan2@u.pacific.edu>
2022-04-02 14:21:16 -0700

    Added key Item to screen.

--- src/main/java/edu/pacific/comp55/starter/DisplayPane.java
+++ src/main/java/edu/pacific/comp55/starter/DisplayPane.java
@@ -18,19 +18,30 @@ public class DisplayPane extends GraphicsPane {
 	private int currentLevel;
 	private HashMap<Integer, GPoint> map;
 	
+	//Class objects
 	private Player player;
+	private Item key; 
 	
 	public DisplayPane(MainApplication app) {
 		super();
 		program = app;
 		//background = new GImage("bow.png", program.getWidth() * 2 / 3, 200);
+		
+		//Add play to the screen and create player object
 		GImage playerSprite = new GImage ("Player-Sprite.png", program.getWidth()/2, program.getHeight()/2);
 		player = new Player(playerSprite, 5);
+		
+		//For player movement
 		map = new HashMap<Integer, GPoint>();
 		map.put(87, new GPoint(0, -5)); //w
 		map.put(65, new GPoint(-5, 0)); //a
 		map.put(83, new GPoint(0, 5)); //s
 		map.put(68, new GPoint(5, 0)); //d
+		
+		//Add key item to the screen.
+		GImage keySprite = new GImage ("keyImage.png", 200, 200); //Create a new sprite for key.
+		keySprite.setSize(25, 25); //Resize sprite to make it smaller.
+		key = new Item(keySprite, "key"); //Create key as Item object.
 	}
 
 	public void setBackground(String b) { //TODO set background
@@ -48,12 +59,15 @@ public class DisplayPane extends GraphicsPane {
 	@Override
 	public void showContents() {
 		//program.add(background);
-		program.add(player.getSprite());
+		program.add(player.getSprite()); //Add player sprite to screen.
+		program.add(key.getImage());
+		
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(player.getSprite());
+		program.remove(key.getImage());
 	}
 	
 	@Override
