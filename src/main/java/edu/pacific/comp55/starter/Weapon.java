53775d5c1f9f2f2ae58f956904ae158ef67e1370
Austin <a_nguyen202@u.pacific.edu>
2022-04-01 20:44:26 -0700

    added constructor and set/get methods

--- src/main/java/edu/pacific/comp55/starter/Weapon.java
+++ src/main/java/edu/pacific/comp55/starter/Weapon.java
@@ -1,10 +1,20 @@
 package edu.pacific.comp55.starter;
 
+import acm.graphics.GImage;
+
 public class Weapon extends Item {
 	private int range;
 	
-	public Weapon() {
-		// TODO Auto-generated constructor stub
+	public Weapon(GImage image, String name) {
+		super(image, name);
+	}
+	
+	public void setRange(int r) {
+		range = r;
+	}
+	
+	public int getRange() {
+		return range;
 	}
 
 	public static void main(String[] args) {
