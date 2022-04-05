cca8e31735a002bd745759b7279d0de9a4c2b927
Austin <a_nguyen202@u.pacific.edu>
2022-04-05 14:26:43 -0700

    removed TODO comment

--- src/main/java/edu/pacific/comp55/starter/Chest.java
+++ src/main/java/edu/pacific/comp55/starter/Chest.java
@@ -6,13 +6,10 @@ public class Chest extends Item {
 	private GImage openChest;
 
 	public Chest(GImage image, String name) { // image is when chest closed
-		// TODO Auto-generated constructor stub
 		super(image, name);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-	//system.out.println("Press E, to retrieve items");
 }
