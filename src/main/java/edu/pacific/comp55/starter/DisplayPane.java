4fb58644d35cb52e4aa1e6e93fd56dcc89fbbbdf
Melkman68 <e_khan2@u.pacific.edu>
2022-04-07 14:05:04 -0700

    Implemented canInteract() for enemy. Enemy moves away from player.

--- src/main/java/edu/pacific/comp55/starter/DisplayPane.java
+++ src/main/java/edu/pacific/comp55/starter/DisplayPane.java
@@ -29,8 +29,6 @@ public class DisplayPane extends GraphicsPane implements ActionListener{
 	//Class objects
 	private Player player;
 	private Enemy enemy;
-	GImage enemySprite = new GImage ("Enemy-sprite.png", 300, 50);
-	GImage playerSprite = new GImage ("knight-sprite4.png", program.getWidth()/2, program.getHeight()/2);
 	private GRect inventoryBox;
 
 	private double dx = 0;
@@ -56,9 +54,11 @@ public class DisplayPane extends GraphicsPane implements ActionListener{
 		itemLabel.put("heart", "Press e to pick up heart.");
 		
 		//Add playerSprite to the screen and create player object.
+		GImage playerSprite = new GImage ("knight-sprite4.png", program.getWidth()/2, program.getHeight()/2);
 		player = new Player(playerSprite, 5);
 		
 		//Add Enemy to screen and create enemy object
+		GImage enemySprite = new GImage ("Enemy-sprite.png", 300, 50);
 		enemy = new Enemy(enemySprite, 5);
 		
 		inventoryBox = new GRect(50, 0, 0, 0);
@@ -101,6 +101,8 @@ public class DisplayPane extends GraphicsPane implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		GImage playerSprite = player.getSprite();
+		GImage enemySprite = enemy.getSprite();
 		timerCount++;
 		if (timerCount % 500 == 0) {
 			dashAvailable = true; //TODO show to player that dash is available
@@ -118,7 +120,9 @@ public class DisplayPane extends GraphicsPane implements ActionListener{
 		}
 		if (enemy.canInteract(player.getSprite().getX(), player.getSprite().getY())) {
 			if (timerCount % 100 == 0) {
-				System.out.println("player in range of enemy");
+				playerSprite = player.getSprite();
+				//System.out.println("player in range of enemy");
+				//System.out.println("playersprite location" + playerSprite.getX() +  " " + playerSprite.getY());
 				enemySprite.move(5, 0);
 			}
 		}
--- src/main/java/edu/pacific/comp55/starter/Enemy.java
+++ src/main/java/edu/pacific/comp55/starter/Enemy.java
@@ -15,7 +15,7 @@ public class Enemy extends Character {
 	public Boolean canInteract(double x, double y) {
 		double xDiff = Math.abs(x - super.getSprite().getX()); // find difference in x coordinates
 		double yDiff = Math.abs(y - super.getSprite().getY()); // find difference in y coordinates
-		return xDiff <= 100 && yDiff <= 100; //returns true if x,y coordinates are within 50 in x direction and y direction
+		return xDiff <= 100 && yDiff <= 100; //returns true if x,y coordinates are within 100 in x direction and y direction
 	}
 	
 	public static void main(String[] args) {
