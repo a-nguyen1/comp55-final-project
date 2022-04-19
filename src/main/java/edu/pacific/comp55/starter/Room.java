4875d4090d178520eefab49ee6ee7a5e1dc7fddf
Melkman68 <e_khan2@u.pacific.edu>
2022-04-19 11:56:15 -0700

    Refractored for adding items and adding enemies

--- src/main/java/edu/pacific/comp55/starter/Room.java
+++ src/main/java/edu/pacific/comp55/starter/Room.java
@@ -60,32 +60,12 @@ public class Room {
 		enemies = new ArrayList<Enemy>(); // initialize enemy array list
 		if (level == 1) {
 			if (room % 3 != 0) { // TODO change
-				GImage enemySprite = new GImage(sprites.get("goblin"), 300, 120);
-				Enemy goblin = new Enemy(enemySprite, 2, "close range goblin");
-				enemies.add(goblin); //add enemy to ArrayList
-				
-				GImage enemySprite2 = new GImage(sprites.get("baby goblin"), 530, 120);
-				Enemy babyGoblin = new Enemy(enemySprite2, 2, "close range baby goblin");
-				enemies.add(babyGoblin); //add enemy2 to ArrayList
-				
-				GImage enemySprite3 = new GImage (sprites.get("flying goblin"), 300, 300);
-				Enemy enemy3 = new Enemy(enemySprite3, 2, "long range flying goblin"); //Enemy has 2 health points.
-				enemy3.setDetectionRange(300);
-				Weapon weapon = new Weapon(new GImage(""), "mouth", 400);
-				enemy3.setWeapon(weapon);
-				enemies.add(enemy3); //add enemy to ArrayList
+				addEnemy("goblin", 300, 120, 2, "close range goblin", 100); //add enemy to ArrayList
+				addEnemy("baby goblin", 530, 120, 2, "close range baby goblin", 100); //add enemy to ArrayList
+				addEnemy("flying goblin", 300, 300, 2, "long range flying goblin", 300, 400); //add enemy to ArrayList
 			}
 			else { // add boss to screen
-				GImage bossSprite = new GImage (sprites.get("dragon"), 300, 120);
-				bossSprite.setSize(bossSprite.getWidth() * 2, bossSprite.getHeight() * 2);
-				Boss boss = new Boss(bossSprite, 5, "long range dragon boss"); //Boss has 5 health points.
-				boss.setSpeed(10);
-				boss.setDetectionRange(800);
-				Weapon weapon = new Weapon(new GImage(""), "mouth", 500);
-				boss.setWeapon(weapon);
-				GImage bullet = new GImage("fireBallSprite.png", boss.getSprite().getX(), boss.getSprite().getY());
-				boss.setBulletSprite(bullet);
-				enemies.add(boss); //add enemy to ArrayList
+				addBoss(); //add enemy to ArrayList
 			}
 		}
 		else if (level == 2) {
@@ -96,6 +76,35 @@ public class Room {
 		}
 		return enemies; 
 	}
+
+	private void addBoss() {
+		GImage bossSprite = new GImage (sprites.get("dragon"), 300, 120);
+		bossSprite.setSize(bossSprite.getWidth() * 2, bossSprite.getHeight() * 2);
+		Boss boss = new Boss(bossSprite, 5, "long range dragon boss"); //Boss has 5 health points.
+		boss.setSpeed(10);
+		boss.setDetectionRange(800);
+		Weapon weapon = new Weapon(new GImage(""), "mouth", 500);
+		boss.setWeapon(weapon);
+		GImage bullet = new GImage("fireBallSprite.png", boss.getSprite().getX(), boss.getSprite().getY());
+		boss.setBulletSprite(bullet);
+		enemies.add(boss);
+	}
+
+	private void addEnemy(String enemy, double x, double y, int health, String enemyType, int detectionRange, int weaponRange) {
+		GImage enemySprite = new GImage(sprites.get(enemy), x, y);
+		Enemy enemyToAdd = new Enemy(enemySprite, health, enemyType);
+		enemyToAdd.setDetectionRange(detectionRange);
+		Weapon weapon = new Weapon(new GImage(""), "", weaponRange);
+		enemyToAdd.setWeapon(weapon);
+		enemies.add(enemyToAdd);
+	}
+
+	private void addEnemy(String enemy, double x, double y, int health, String enemyType, int detectionRange) {
+		GImage enemySprite = new GImage(sprites.get(enemy), x, y);
+		Enemy enemyToAdd = new Enemy(enemySprite, health, enemyType);
+		enemyToAdd.setDetectionRange(detectionRange);
+		enemies.add(enemyToAdd);
+	}
 	
 	public ArrayList<Item> getItems() {
 		items = new ArrayList<Item>(); // initialize item array list
@@ -104,29 +113,30 @@ public class Room {
 		double x = 100 + Math.random() * (width - 200); //randomize x so not at edge of screen (offset by 100)
 		double y = 100 + Math.random() * (height - 200); //randomize y so not at edge of screen (offset by 100)
 		
-		GImage keySprite = new GImage (sprites.get("key"), x, y); //Create a new sprite for key.
-		keySprite.setSize(25, 25); //Resize sprite to make it smaller.
-		PickUpItem key = new PickUpItem(keySprite, "key"); //Create key as Item object.
-		items.add(key);
-		
-		//create heart object
-		GImage heartSprite = new GImage (sprites.get("heart"), 150, 300); //Create a new sprite for heart.
-		heartSprite.setSize(25, 25); //Resize sprite to make it smaller.
-		PickUpItem heart = new PickUpItem(heartSprite, "heart");
-		items.add(heart);
-				
-		//create chest object
-		GImage chestSprite = new GImage (sprites.get("chest"), 500, 200);
-		chestSprite.setSize(25, 25);
-		Chest chest = new Chest(chestSprite, "chest");
-		items.add(chest);
-		
-		//create door object
-		GImage doorSprite = new GImage (sprites.get("door"), 300, 100); //Create a new sprite for door.
-		Door door = new Door(doorSprite, "closedDoor"); //Create door as Item object.
-		items.add(door);
+		addItem("heart", 150, 300);
+		addItem("chest", 500, 200);
+		addItem("door", 300, 100);
+		addItem("key", x, y);
 		
 		return items; 
+	}
+
+	private void addItem(String itemName, double x, double y) {
+		GImage itemSprite = new GImage (sprites.get(itemName), x, y); //Create a new sprite for key.
+		if (itemName == "key" || itemName == "heart" ) {
+			itemSprite.setSize(25, 25); //Resize sprite to make it smaller.
+			PickUpItem pickUp = new PickUpItem(itemSprite, itemName); //Create key as Item object.
+			items.add(pickUp);
+		}
+		else if (itemName == "chest") {
+			itemSprite.setSize(25, 25);
+			Chest chest = new Chest(itemSprite, "chest");
+			items.add(chest);
+		}
+		else if (itemName == "door") {
+			Door door = new Door(itemSprite, "closedDoor"); //Create door as Item object.
+			items.add(door);
+		}
 	}
 
 	public static void main(String[] args) {
